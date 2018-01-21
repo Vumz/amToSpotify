0c87eca133ff1ae0f8d28bd94c5b183b07e321e9
VamseeG <vamsee.gangaram@gmail.com>
2018-01-21 12:28:33 -0500

    heroku test

--- app.py
+++ app.py
@@ -1,45 +1,15 @@
-from flask import Flask, request, redirect, url_for
-import auth
-import envVariables as eVar
-import webbrowser
-import ast
-import processing
-
-
-
-itunesXMLFile = 'itunes Music Library.xml'
-playlistApple = 'Drive'
-playlistSpotify = 'Drive'
-
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    #directs the user to the spotify authentication url to request permission
-    authURL = auth.getAuthURL()
-    return redirect(authURL)
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-@app.route('/login')
-def login():
-    #gets token data from the authentication callback
-    tokenData = auth.getAuthTokens()
-    return redirect(url_for('convert', tokenData=tokenData))
-
-@app.route('/convert')
-def convert():
-    try:
-        #get the tokenData from the url
-        tokenData = ast.literal_eval(request.args.get('tokenData'))
-    except ValueError as v:
-        #if user cancels spotify authentication, return them to the authentication page again
-        return redirect('http://127.0.0.1:8080/')
-    accessHeader = auth.getAccessHeader(tokenData)
-    itunesSongs = processing.getAppleMusic(itunesXMLFile, playlistApple)
-    trackURIs = processing.getTrackURIs(itunesSongs, accessHeader)
-    if processing.addToPlaylist(trackURIs, playlistSpotify, accessHeader):
-        return "Success"
-    return "Failed"
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    """.format(time=the_time)
 
 if __name__ == '__main__':
-    # webbrowser.open('http://127.0.0.1:8080/')
     app.run(debug=True, use_reloader=True)
